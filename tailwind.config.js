f435a4fa4b816c1a66024109e0b2766f5c749b39
Thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-05-09 11:18:07 +0700

    header & footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily : {
+      nunito : ['Nunito Sans', 'sans-serif'],
+      sans : ['Source Sans Pro', 'sans-serif'],
+    },
+    fontSize: {
+      'font-10': '0.625rem',
+      'font-14': '0.875rem',
+      'font-18': '1.125rem',
+      'font-20': '1.25rem',
+      'font-30': '1.875rem',
+      'font-32': '2rem'
+    },
+    colors : {
+      'color-green-light': '#1dbf73',
+      'color-green':'#19a463',
+      'color-black': '#222325',
+      'color-black-light': '#404145',
+      'color-gray': '#74767e',
+      'color-gray-light': '#62646a',
+      'color-gray-light-2': '#b5b6ba',
+      'color-white': '#fff',
+      'color-white-light' : '#e4e5e7',
+      'color-green-dark': '#003912',
+      'color-puple-dark': '#0d084d',
+      'color-puple':' #1e1692',
+      'color-puple-light': '#446ee7',
+    }
   },
   plugins: [],
 }
 
+
+  //font
